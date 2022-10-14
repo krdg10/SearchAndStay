22a2e7bd431e5014bd9629c75dcd16391110f84c
krdg10 <krdg10@live.com>
2022-10-14 04:22:40 -0300

    navbar created

---
+++ components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <b-navbar toggleable="sm" type="dark" variant="dark" class="mb-5">
+        <NuxtLink to="/">
+            <b-navbar-brand>Search And Stay</b-navbar-brand>
+        </NuxtLink>
+    </b-navbar>
+</template>
